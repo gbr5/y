c4ee5328440c362a4408e0a0ddb1056d33aff43f
Guy Birchal Robbe - Mac <gbrobbe@gmail.com>
2024-11-11 20:03:03 -0300

    FIX: type errors

--- src/dtos/PostReply.ts
+++ src/dtos/PostReply.ts
@@ -3,7 +3,8 @@ import { TProfile } from "./Profile"
 export type TPostReply = {
   id: string
   user_id: string
-  tweet_id: string
+  tweet_id: string | null
+  reply_id: string | null
   text: string
   profiles?: TProfile | null
   // replies: TPostReply[] | null
